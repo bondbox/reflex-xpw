6267c14e34ebe4d93a1584fa1d83e5ca1e453ea6
Mingzhe Zou <zoumingzhe@qq.com>
2026-07-07 17:03:58 +0800

    bump version from 0.2 to 0.3

--- rxpw_backend/attribute.py
+++ rxpw_backend/attribute.py
@@ -6,6 +6,6 @@ __project_home__ = "https://github.com/bondbox/reflex-xpw/"
 __project_desc__ = "Simple access control for Reflex applications based on xpw"
 
 # package info
-__version__ = "0.2"
+__version__ = "0.3"
 __package_name__ = "reflex-xpw-backend"
 __package_desc__ = "Backend for Reflex applications based on xpw"
--- rxpw_frontend/attribute.py
+++ rxpw_frontend/attribute.py
@@ -6,6 +6,6 @@ __project_home__ = "https://github.com/bondbox/reflex-xpw/"
 __project_desc__ = "Simple access control for Reflex applications based on xpw"
 
 # package info
-__version__ = "0.2"
+__version__ = "0.3"
 __package_name__ = "reflex-xpw-frontend"
 __package_desc__ = "Frontend for Reflex applications based on xpw"
